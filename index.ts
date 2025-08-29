f86742ae794a8ac8fccee8d4fea4155a475c68ba
Lakshitha Madumal <75256642+lakshithamadumal@users.noreply.github.com>
2025-08-29 11:37:04 +0530

    Util Avatar

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { registerRootComponent } from 'expo';
 
-import App from './LoginScreen';
+import App from './SignupScreen';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
