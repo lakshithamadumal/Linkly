94e57276bd1677ff131790e11d030af00200d5da
Lakshitha Madumal <75256642+lakshithamadumal@users.noreply.github.com>
2025-08-30 11:26:34 +0530

    App Update

--- App.tsx
+++ App.tsx
@@ -13,7 +13,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Splash" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Splash" component={SplashScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="SignUp" component={SignupScreen} />
